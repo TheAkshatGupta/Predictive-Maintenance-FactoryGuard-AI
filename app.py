1701da336be327b6e9d12b31f977400885f87bb4
TheAkshatGupta <2k23.csds2310601@gmail.com>
2026-03-13 00:14:16 +0530

    Add FastAPI backend for Render deployment

---
+++ app.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import numpy as np
+
+app = FastAPI()
+
+model = joblib.load("model.pkl")
+
+class MachineData(BaseModel):
+    air_temp: float
+    process_temp: float
+    rpm: float
+    torque: float
+    wear: float
+
+@app.get("/")
+def home():
+    return {"message": "FactoryGuard AI API Running"}
+
+@app.post("/predict")
+def predict(data: MachineData):
+
+    features = np.array([[
+
+        data.air_temp,
+        data.process_temp,
+        data.rpm,
+        data.torque,
+        data.wear
+
+    ]])
+
+    prediction = model.predict(features)
+
+    if prediction[0] == 1:
+        result = "⚠️ Machine Failure Predicted"
+    else:
+        result = "✅ Normal Machine Operation"
+
+    return {"prediction": result}
