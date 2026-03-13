7b2e69670e037109138346bec12307e0bd0ec89b
TheAkshatGupta <2k23.csds2310601@gmail.com>
2026-03-13 13:15:41 +0530

    Connect frontend to Render API

--- frontend/script.js
+++ frontend/script.js
@@ -1,17 +1,29 @@
+document.querySelectorAll("input").forEach(slider=>{
+
+slider.oninput=function(){
+
+document.getElementById(this.id+"_value").innerText=this.value
+
+}
+
+})
+
 async function predict(){
 
-let air = document.getElementById("air_temp").value
-let process = document.getElementById("process_temp").value
-let rpm = document.getElementById("rpm").value
-let torque = document.getElementById("torque").value
-let wear = document.getElementById("wear").value
+let air=document.getElementById("air_temp").value
+let process=document.getElementById("process_temp").value
+let rpm=document.getElementById("rpm").value
+let torque=document.getElementById("torque").value
+let wear=document.getElementById("wear").value
 
-let response = await fetch("/api/predict",{
+let response=await fetch("https://factoryguard-ai-5slb.onrender.com",{
 
 method:"POST",
 
 headers:{
+
 "Content-Type":"application/json"
+
 },
 
 body:JSON.stringify({
@@ -26,8 +38,26 @@ wear:wear
 
 })
 
-let data = await response.json()
+let data=await response.json()
 
-document.getElementById("status").innerText = data.prediction
+document.getElementById("status").innerText=data.prediction
+
+let meter=document.getElementById("meter-fill")
+
+let health=document.getElementById("health-text")
+
+if(data.prediction.includes("Failure")){
+
+meter.style.width="90%"
+health.innerText="Machine Health: Critical"
+
+}else{
+
+meter.style.width="30%"
+health.innerText="Machine Health: Stable"
 
 }
+
+}
+
+particlesJS.load('particles-js','https://cdn.jsdelivr.net/gh/VincentGarreau/particles.js/particles.json')
