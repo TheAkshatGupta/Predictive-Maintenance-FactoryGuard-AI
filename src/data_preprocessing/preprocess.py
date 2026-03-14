9668bff90857d86700e27fc5eb05055a9575b3df
TheAkshatGupta <2k23.csds2310601@gmail.com>
2026-03-14 14:52:01 +0530

    Clean project structure and remove unnecessary preprocessing files

--- src/data_preprocessing/preprocess.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("data/ai4i_predictive_maintenance.csv")  
-df = df.drop_duplicates()
-df = df.dropna()
-df.to_csv("data/cleaned_data.csv", index=False)
-print("Cleaned dataset saved")
